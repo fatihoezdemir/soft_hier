0df8cfeed66eb0852ea4c11ad2d261a6d4209d9b
fatihoezdemir <ozdemir@posteo.de>
2025-12-14 22:18:54 +0000

    kernel configuration scfript make it run with deisabled numerical check(bad commit, fix )

--- examples/SoftHier/assembled/DQ/scripts/utils/kernel_configuration.py
+++ examples/SoftHier/assembled/DQ/scripts/utils/kernel_configuration.py
@@ -62,8 +62,8 @@ def generate_config_C_header(header_prefix, config, C_header_file, dtype, numeri
             
             else:
                 file.write(f'#define REDMULE_COMPUTE_TYPE        REDMULE_NONE_16\n')
-                file.write(f'#define COLLECTIVE_REDSUM_TYPE      COLLECTIVE_REDADD_NONE\n')
-                file.write(f'#define COLLECTIVE_REDMAX_TYPE      COLLECTIVE_REDMAX_NONE\n')
+                file.write(f'#define COLLECTIVE_REDSUM_TYPE      COLLECTIVE_REDADD_FP_16\n')
+                file.write(f'#define COLLECTIVE_REDMAX_TYPE      COLLECTIVE_REDADD_FP_16\n')
         elif dtype == 'fp8':
             file.write(f'#define {header_prefix.upper()}_FP8\n')
             file.write(f'#define DATA_TYPE_WIDTH             8\n')
@@ -71,12 +71,12 @@ def generate_config_C_header(header_prefix, config, C_header_file, dtype, numeri
             file.write(f'typedef uint8_t                     {header_prefix.lower()}_data_t;\n')
             if numerical_check == 1:
                 file.write(f'#define REDMULE_COMPUTE_TYPE        REDMULE_FP_8\n')
-                file.write(f'#define COLLECTIVE_REDSUM_TYPE      COLLECTIVE_REDADD_FP_8\n')
-                file.write(f'#define COLLECTIVE_REDMAX_TYPE      COLLECTIVE_REDMAX_FP_8\n')
+                file.write(f'#define COLLECTIVE_REDSUM_TYPE      COLLECTIVE_REDADD_FP_16\n')
+                file.write(f'#define COLLECTIVE_REDMAX_TYPE      COLLECTIVE_REDADD_FP_16\n')
             else:
                 file.write(f'#define REDMULE_COMPUTE_TYPE        REDMULE_UINT_8\n')
-                file.write(f'#define COLLECTIVE_REDSUM_TYPE      COLLECTIVE_REDADD_NONE\n')
-                file.write(f'#define COLLECTIVE_REDMAX_TYPE      COLLECTIVE_REDMAX_NONE\n')
+                file.write(f'#define COLLECTIVE_REDSUM_TYPE      COLLECTIVE_REDADD_FP_16\n')
+                file.write(f'#define COLLECTIVE_REDMAX_TYPE      COLLECTIVE_REDADD_FP_16\n')
 
         if appendix is not None:
             for string in appendix:
